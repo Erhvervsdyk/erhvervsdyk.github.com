ae2b2f563ed001bb602a8418b42e3e6d215f3a31
Daniel Aakjær Sørensen <daniel.aakjaer@gmail.com>
2025-12-28 09:37:02 +0100

    Adding sitemap.xml for Google

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://erhvervsdyk.dk",
+      dynamicRoutes: ["/"],
+    }),
+  ],
   base: "./",
   build: {
     outDir: "build",
